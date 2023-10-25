3478d71cdb070a5ef6f682b57d87467e29b49fd8
fatimaminhas <f.minhas394@gmail.com>
2023-10-25 17:12:12 -0400

    added readme

--- models/Thought.js
+++ models/Thought.js
@@ -1,27 +1,27 @@
-const { Schema, model } = require('mongoose');
+// const { Schema, model } = require('mongoose');
 
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            maxlength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            //getter
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reactions: [
-            reactionSchema,
-        ],
-    });
+// const thoughtSchema = new Schema(
+//     {
+//         thoughtText: {
+//             type: String,
+//             required: true,
+//             maxlength: 280,
+//         },
+//         createdAt: {
+//             type: Date,
+//             default: Date.now,
+//             //getter
+//         },
+//         username: {
+//             type: String,
+//             required: true,
+//         },
+//         reactions: [
+//             reactionSchema,
+//         ],
+//     });
 
-// Initialize our Post model
-const Thought = model('thought', thoughtSchema);
+// // Initialize our Post model
+// const Thought = model('thought', thoughtSchema);
 
-module.exports = Thought;
+// module.exports = Thought;
